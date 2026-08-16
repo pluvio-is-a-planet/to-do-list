fc0e40eb9606f1546d46d4c77edf83d31131ee38
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-16 20:30:53 +0200

    Add observer/listener logic to Collection

--- src/components/collection.js
+++ src/components/collection.js
@@ -1,7 +1,8 @@
 class Collection {
-    constructor(itemClass, itemArray) {
+    constructor(itemClass, itemArray, onChange = () => {}) {
         this._itemClass = itemClass;
         this._itemArray = itemArray;
+        this._listeners = [onChange];
     }
 
     get all() {
@@ -13,6 +14,7 @@ class Collection {
     add(options) {
         const newItem = new this._itemClass(options);
         this._itemArray.push(newItem);
+        this._notify();
         return newItem;
     }
 
@@ -24,15 +26,24 @@ class Collection {
         const index = this._itemArray.findIndex(entry => entry.id === id);
         if (index === -1) return null;
         const removedItem = this._itemArray.splice(index, 1)[0];
+        this._notify();
         return removedItem;
+    }
+
+    subscribe(listener) {
+        this._listeners.push(listener);
+    }
+
+    _notify() {
+        this._listeners.forEach(func => func());
     }
 
     toJSON() {
         return this._itemArray.map(entry => entry.toJSON());
     }
 
-    static fromJSON(itemClass, itemArray) {
-        return new this(itemClass, itemArray.map(entry => itemClass.fromJSON(entry)));
+    static fromJSON(itemClass, itemArray, onChange) {
+        return new this(itemClass, itemArray.map(entry => itemClass.fromJSON(entry)), onChange);
     }
 }
 
