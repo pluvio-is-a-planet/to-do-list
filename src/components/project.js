2115b1247834fc00e1371e29f6e9a84f20267502
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-16 19:23:05 +0200

    Add Project class

---
+++ src/components/project.js
@@ -0,0 +1,25 @@
+class Project {
+    constructor({id, title}) {
+        this._id = id ?? crypto.randomUUID();
+        this._title = title;
+    }
+
+    get title() { return this._title }
+
+    update({title}) {
+        this._title = title;
+    }
+
+    toJSON() {
+        return {
+            id: this._id,
+            title: this._title,
+        };
+    }
+
+    static fromJSON(options) {
+        return new this(options);
+    }
+}
+
+export default Project;
