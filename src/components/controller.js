fc74a3541f62b2c925930a1212cbaa631e5e2330
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-16 19:43:48 +0200

    Add persistent storage logic

--- src/components/controller.js
+++ src/components/controller.js
@@ -4,11 +4,17 @@ import Project from "./project.js";
 import Task from "./task.js";
 import Note from "./note.js";
 
+import { save, load } from "../logic/local-storage.js";
+
 const controller = (() => {
     // I plan to initialize these collections from localStorage using a separate save/load module
-    const _projects = Collection.fromJSON(Project, []);
-    const _tasks = Collection.fromJSON(Task, []);
-    const _notes = Collection.fromJSON(Note, []);
+    const _projects = Collection.fromJSON(Project, load("projects"));
+    const _tasks = Collection.fromJSON(Task, load("tasks"));
+    const _notes = Collection.fromJSON(Note, load("notes"));
+
+    return {
+        // necessary methods
+    }
 })();
 
 export default controller;
