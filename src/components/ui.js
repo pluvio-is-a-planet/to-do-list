5a98e4c6920233737cdb2280ae2eaae5ce29baa1
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-17 19:02:17 +0200

    Add delete button with icon to projects in project list

--- src/components/ui.js
+++ src/components/ui.js
@@ -1,6 +1,8 @@
 import controller from "./controller.js";
 
-import { createCustomElement } from "../logic/helpers.js";
+import { createCustomElement, createImageElement } from "../logic/helpers.js";
+
+import deleteIcon from "../assets/icons/delete.svg";
 
 const ui = (() => {
     // --- project setup ---
@@ -8,36 +10,55 @@ const ui = (() => {
     const sidebarProjects = document.querySelector(".categories .projects");
     const projectList = sidebarProjects.querySelector("ul");
     const projectForm = document.querySelector(".actions .project-form");
+    const titleInput = projectForm.querySelector("input#project-title");
     const cancelProjectBtn = projectForm.querySelector(".cancel-btn");
 
     renderProjectList();
     controller.onUpdate(renderProjectList);
 
-    sidebarProjects.addEventListener("click", e => projectList.classList.toggle("hidden"));
+    sidebarProjects.addEventListener("click", e => {
+        const target = e.target.closest(".category-header");
+        if (e.target === target) projectList.classList.toggle("hidden");
+    });
+    
     projectList.addEventListener("click", e => {
-        const target = e.target.closest("h3");
-        renderProjectContent(controller.projects.find(target.dataset.id));
+        // const target = e.target.closest("h3");
+        // renderProjectContent(controller.projects.find(target.dataset.id));
 
     });
 
     const projectFormBtn = document.querySelector(".project-form-btn");
-    projectFormBtn.addEventListener("click", toggleProjectForm);
+    projectFormBtn.addEventListener("click", e => {
+        toggleProjectForm();
+        titleInput.focus();
+    });
     cancelProjectBtn.addEventListener("click", toggleProjectForm);
 
     projectForm.addEventListener("submit", e => {
         e.preventDefault();
-        const titleInput = projectForm.querySelector("input#project-title");
         controller.projects.add({title: titleInput.value});
+        projectForm.reset();
+        titleInput.focus();
     });
 
     function renderProjectList() {
+        projectList.replaceChildren();
         const projects = controller.projects.all;
         projects.forEach(entry => {
             console.log(entry.title);
             const listItem = createCustomElement("li", "project");
             const title = createCustomElement("h3", "project-title", entry.title);
+            const deleteBtn = createCustomElement("button", "delete-project", "");
+            deleteBtn.appendChild(
+                createImageElement(deleteIcon)
+            );
+
             title.dataset.id = entry.id;
+
+            deleteBtn.addEventListener("click", e => controller.projects.remove(title.dataset.id));
+
             listItem.appendChild(title);
+            listItem.appendChild(deleteBtn);
 
             projectList.appendChild(listItem);
         });
